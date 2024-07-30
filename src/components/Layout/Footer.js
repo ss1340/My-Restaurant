805c5d018ee5ea0f71a04a53a9811e8647cd82e3
Satyam Singh <115134729+ss1340@users.noreply.github.com>
2024-07-31 01:36:30 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
           <InstagramIcon />
           <TwitterIcon />
           <GitHubIcon />
-          {/* <YouTubeIcon /> */}
+          
         </Box>
         <Typography
           variant="h5"
